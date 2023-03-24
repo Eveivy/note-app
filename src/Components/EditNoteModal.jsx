a41a5c6a3627ce1f5054e9e2720d7e85d1484d21
Isaac Evelyn <evelyn.isaac@cinfores.com>
2023-03-25 01:23:29 +1300

    implementing note preview and delete

--- src/Components/EditNoteModal.jsx
+++ src/Components/EditNoteModal.jsx
@@ -7,51 +7,65 @@ import Col from 'react-bootstrap/Col';
 // import { useFormik } from 'formik';
 import 'boxicons'
 
-export default function EditNoteModal({ showEditModal, closeEditModal, ...otherProps}) {
+export default function EditNoteModal({ showEditModal, closeEditModal, ...otherProps }) {
     const date = otherProps.today.getFullYear() + '-' + (otherProps.today.getMonth() + 1) + '-' + otherProps.today.getDate();
     const time = otherProps.today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
     const dateTime = date + ' | ' + time;
 
-   
-    const editData  = {
-        title: otherProps.vals.title,
-        note: otherProps.vals.note
+    const [visibility, setVisibility] = useState(true)
+
+    let editData = otherProps.vals
+    //  console.log(editData)
+
+    const [editedVals, setEditedVals] = useState({
+        t: editData.title,
+        n: '',
+        td: dateTime
+    })
+    // console.log(editedVals.t)
+    const handleEdit = () => {
+
     }
-    // console.log(otherProps.vals.title)
-
-    const handleEdit = ({ target, value }) => {
-        // setEditData(prev => {
-        //     return {
-        //         ...prev,
-        //         [target.name]: value
-        //     }
-        // })
-    }
 
 
-    
+
     return (
         <>
             <Modal show={showEditModal} onHide={closeEditModal}>
                 <Modal.Body>
-                    <Form> 
+                    {visibility ?
                         <Row>
                             <Col xl={12} className="mt-3">
                                 <small className='font-main text-muted text-lowercase'>{otherProps.vals.dateTime}</small>
                             </Col>
                             <Col xl={12} className="mt-3 rounded-pill" style={{ backgroundColor: "#261f47" }}>
-                                <Form className='border-0 w-100 p-2 text-white' as="input" name="title" onChange={handleEdit} value={editData.title} placeholder="Note title" />
+                                <div className='border-0 w-100 p-2 text-white>
+                                  
+                                </div>
                             </Col>
                             <Col xl={12} className="mt-3">
-                                <Form className='border-0 w-100 p-2' as="textarea" rows={15} name="note" placeholder='Note down something' onChange={handleEdit} value={editData.note} />
+                                <div className='border-0 w-100 p-2 text-white></div>
                             </Col>
-                            <Col xl={12} className="mt-4 d-flex align-items-end justify-content-end p-3">
-                                <Button type="submit" className='me-3 rounded-circle d-flex align-items-center py-2 px-2' style={{ backgroundColor: "#261f47" }}>
-                                    <box-icon name='check' size="2rem" color="white" ></box-icon>
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Form>
+
+                        </Row> : 
+                        <Form>
+                            <Row>
+                                <Col xl={12} className="mt-3">
+                                    <small className='font-main text-muted text-lowercase'>{otherProps.vals.dateTime}</small>
+                                </Col>
+                                <Col xl={12} className="mt-3 rounded-pill" style={{ backgroundColor: "#261f47" }}>
+                                    <Form className='border-0 w-100 p-2 text-white' as="input" name="title" onChange={handleEdit} value={editData.title} placeholder="Note title" />
+                                </Col>
+                                <Col xl={12} className="mt-3">
+                                    <Form className='border-0 w-100 p-2' as="textarea" rows={15} name="note" placeholder='Note down something' onChange={handleEdit} value={editData.note} />
+                                </Col>
+                                <Col xl={12} className="mt-4 d-flex align-items-end justify-content-end p-3">
+                                    <Button type="submit" className='me-3 rounded-circle d-flex align-items-center py-2 px-2' style={{ backgroundColor: "#261f47" }}>
+                                        <box-icon name='check' size="2rem" color="white" ></box-icon>
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Form>}
                 </Modal.Body>
 
             </Modal>
